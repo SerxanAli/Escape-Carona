70b5b3fe7dfc6c59308d960fe0897fc72c351cfc
SerxanAli <aliyev.s.v1990@gmail.com>
2023-04-10 15:19:27 +0400

    1.05

--- Escape Carona/GameScene.swift
+++ Escape Carona/GameScene.swift
@@ -24,11 +24,19 @@ class GameScene: SKScene {
     var dokunmaKontrol = false
     
     var timer: Timer?
+    var zaman: Timer?
+    
+    
+    var Score = 50 {
+        didSet {
+            scoreLabel.text = "Score: \(Score)"
+        }
+    }
     
     
     override func didMove(to view: SKView) {
         
-      
+        
         
         if let tempKarakter = self.childNode(withName: "pacman") as? SKSpriteNode {
             pacman = tempKarakter
@@ -52,43 +60,89 @@ class GameScene: SKScene {
         
         if let tempKarakter = self.childNode(withName: "scoreLabel") as? SKLabelNode {
             scoreLabel = tempKarakter
-            scoreLabel.text = "xan"
+            
         }
         
-        timer = Timer.scheduledTimer(timeInterval: 0.02, target: self, selector: #selector(GameScene.haraket), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 0.02, target: self, selector: #selector(haraket), userInfo: nil, repeats: true)
         
-     
+        
+        zaman = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(zamanAxisi), userInfo: nil, repeats: true)
+    }
+    
+    @objc func zamanAxisi(){
+        
+        Score = Score - 1
+        
+        if Score < 0 {
+            
+            timer?.invalidate()
+            zaman?.invalidate()
+            
+            self.viewController?.performSegue(withIdentifier: "page3", sender: nil)
+        }
+        
     }
     
     @objc func haraket() {
         
         if dokunmaKontrol {
-           
-            pacman.run(SKAction.moveBy(x: 0, y: +20, duration: 1))
+            
+            let yuxari = SKAction.moveBy(x: 0, y: +20, duration: 1)
+            pacman.run(yuxari)
             
         } else {
             
-            pacman.run(SKAction.moveBy(x: 0, y: -20, duration: 1))
+            let asagi = SKAction.moveBy(x: 0, y: -20, duration: 1)
+            pacman.run(asagi)
         }
-             
+        
+        
+        hereket(obje: corona)
+        hereket(obje: elma)
+        
+        hereket(obje: meyveler)
+        hereket(obje: visne)
+        
+        
     }
     
     
+    func hereket(obje:SKSpriteNode){
+        
+        var yer = obje.position.x
+        
+        if yer < 0 {
+            yer = 900
+            obje.position.x = yer
+            obje.position.y = CGFloat(Float.random(in: 10...1250))
+        } else {
+            yer -= 10
+            obje.position.x = yer
+        }
+        
+        
+    }
+    
+
     func touchDown(atPoint pos : CGPoint) {
         
         dokunmaKontrol = true
+        print(pos.y)
         
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-      
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-      
+        
         dokunmaKontrol = false
-       
+        
     }
+    
+ 
+    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
